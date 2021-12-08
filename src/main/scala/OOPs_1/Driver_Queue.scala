2304a78792d9d5f454f56c604246771c364a25af
Utkarsh Ambastha <utkarsh.ambastha@knoldus.com>
2021-12-08 20:58:04 +0530

    Modified Code

--- src/main/scala/OOPs_1/Driver_Queue.scala
+++ src/main/scala/OOPs_1/Driver_Queue.scala
@@ -9,14 +9,19 @@ object Driver_Queue extends App {
   for (i <- list.indices) println(list(i)+ " " )
 
 
-  var double_list = new Double_Queue(list)           //This will double the elements in the queue
+  var double_list = new Double_Queue(list)                                     //This will double the elements in the queue
   println("\n After doubling the elements in the Queue: ")
   for (i <- list.indices) println(list(i)+ " ")
 
 
-  var squareQueue = new Square_Queue (list)       //Squaring the elements in the queue
+  var squareQueue = new Square_Queue (list)                                     //Squaring the elements in the queue
   println("\n After squaring the elements in the Queue: ")
   for (i <- list.indices) println(list(i)+ " ")
 
   squareQueue.dequeue(list)
+  println("\nAfter dequeue a element in Queue: ")
+  for( i <- list.indices) print(list(i)+" ")
+  squareQueue.enqueue(list)
+  println("\nAfter a element is enqueued: ")
+  for( i <- list.indices) print(list(i)+" ")
 }
